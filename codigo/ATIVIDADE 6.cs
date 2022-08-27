bd90b9fe14c5a815244cb978408361e24b1f59e1
Vinícius de Andrade <74668845+vinicandradd@users.noreply.github.com>
2022-08-26 23:36:23 -0300

    Add files via upload

---
+++ codigo/ATIVIDADE 6.cs
@@ -0,0 +1,25 @@
+﻿namespace ATIVIDADE_6
+{
+    internal class Program
+    {
+        static int Potencia(ref int x)
+        {
+            int y;
+            Console.WriteLine("Digite o expoente");
+            y = int.Parse(Console.ReadLine());
+            int resultado=1;
+            for(int i = 1; i <= y; i++)
+            {
+                resultado *= x;
+            }
+            return resultado;
+        }
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Digite a base");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine(Potencia(ref a));
+        }
+    }
+}
