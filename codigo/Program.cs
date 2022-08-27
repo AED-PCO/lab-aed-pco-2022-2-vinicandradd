1ce489485abf3a70aff8b8ee2a9dff7648b91807
Vinícius de Andrade <74668845+vinicandradd@users.noreply.github.com>
2022-08-26 23:21:08 -0300

    Add files via upload

---
+++ codigo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ATIVIDADE_1
+{
+    internal class Program
+    {
+        static int perfeito(int x)
+        {
+            int soma, primeiro, segundo, quadrado;
+            primeiro = x / 100;
+            segundo = x % 100;
+            soma = primeiro + segundo;
+            quadrado = soma * soma;
+            return quadrado;
+        }
+        static void Main(string[] args)
+        {
+            for(int i = 1000; i <= 9999; i++)
+            {
+                perfeito(i);
+                if (perfeito(i) == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
